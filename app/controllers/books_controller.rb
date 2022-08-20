fced1f86dd186de25c57422ee3f9e10403139d80
na08skt <sakata.na08@gmail.com>
2022-08-20 11:24:27 +0000

    books/index books/show 完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,15 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
   def new
+
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
       flash[:notice] = "投稿できました。"
-      redirect_to book_path(@book) || root_path
+      redirect_to book_path(@new_book) || root_path
     else
       flash[:alert] = "投稿ができませんでした。"
       redirect_to request.referrer || root_path
@@ -16,10 +17,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    @new_book = Book.new
+    @all_books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.create
   end
 
   def edit
@@ -30,12 +34,18 @@ class BooksController < ApplicationController
   end
 
   def destroy
-
+    if @delte_book = Book.find(params[:id])
+      flash[:notice] = "正常に削除できました"
+      redirect_to books_path || root_path
+    else
+      flash[:notice] = "削除できませんでした"
+      redirect_to request.referrer || books_path
+    end
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :image)
   end
 
 end
